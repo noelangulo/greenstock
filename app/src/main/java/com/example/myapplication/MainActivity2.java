b2dbab3b0a21e1abb13a8115977d2c3712b40b96
Noel10 <spark2048@hotmail.com>
2019-06-23 20:50:30 +0200

    AAAA

--- app/src/main/java/com/example/myapplication/MainActivity2.java
+++ app/src/main/java/com/example/myapplication/MainActivity2.java
@@ -9,6 +9,6 @@ public class MainActivity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //·F
+        //NOEL AAAAA
     }
 }
