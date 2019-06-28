69128f6a0ccb9453d8b7032b7f73250f32f6937c
Noel10 <spark2048@hotmail.com>
2019-06-28 13:04:58 +0200

    Segundo Commit

--- app/src/main/java/com/example/myapplication/MainActivity2.java
+++ app/src/main/java/com/example/myapplication/MainActivity2.java
@@ -9,6 +9,6 @@ public class MainActivity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //NOEL AAAAA
+        //NOEL A
     }
 }
